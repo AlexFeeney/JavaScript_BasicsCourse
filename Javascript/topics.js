8bbd8763a9270b0236949060c6c2c5ba3efd8490
AlexFeeney <alefeeney2020@gmail.com>
2024-01-28 12:00:04 +1030

    adding js function

--- Javascript/topics.js
+++ Javascript/topics.js
@@ -30,12 +30,11 @@ const PI = 3.14;
 let radius; 23
 let circumference; 
 
-
-radius = Number(radius); 
-
-circumference = 2 * PI * radius; 
-
-
+//create a function when click submit button
 document.getElementById("submitTwo").onclick = function(){
-    
+    radius = document.getElementById("myText").value;
+    radius = Number(radius); 
+    circumference = 2 * PI * radius; 
+    document.getElementById("myH3Two")
+
 }; 
